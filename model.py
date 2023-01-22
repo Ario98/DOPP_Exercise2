20858218cb50b919c80312d815f7ffcf42e9ca0e
Ario98 <bliteum@protonmail.com>
2023-01-22 20:35:41 +0100

    Updated the pipeline adapted metric to CV

--- model.py
+++ model.py
@@ -7,6 +7,9 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import cross_val_score
+from sklearn.decomposition import PCA
+from xgboost.sklearn import XGBRegressor
+from numpy import sqrt
 
 # Read the data
 df = pd.read_csv("data/preprocessed_full.csv")
@@ -19,7 +22,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Create the pipeline
 pipe = Pipeline([
     ('scaler', StandardScaler()),
-    ('regressor', GradientBoostingRegressor())
+    ('pca', PCA(n_components=0.95)),
+    ('regressor', LinearRegression())
 ])
 
 # Fit the pipeline to the training data
@@ -29,13 +33,15 @@ pipe.fit(X_train, y_train)
 y_pred = pipe.predict(X_test)
 
 # Measure the performance of the model
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-rmse = mean_squared_error(y_test, y_pred, squared=False)
 r2 = cross_val_score(pipe, X, y, cv=5, scoring='r2')
+mse = cross_val_score(pipe, X, y, cv=5, scoring='neg_mean_squared_error')
+mae = cross_val_score(pipe, X, y, cv=5, scoring='neg_mean_absolute_error')
 
-print("Metrics for GradientBoostingRegressor")
-print(f"Mean Absolute Error: {mae:.2f}")
-print(f"Mean Squared Error: {mse:.2f}")
-print(f"Root Mean Squared Error: {rmse:.2f}")
-print("Average CV Score: {:.2f}".format(r2.mean()))
+# Define the steps names for later use
+scaler_name = type(pipe.named_steps['scaler']).__name__
+model_name = type(pipe.named_steps['regressor']).__name__
+
+print("Metrics for Model: {} using Scaler: {}".format(model_name, scaler_name))
+print("Average CV Score (r2): {:.2f}".format(r2.mean()))
+print("Average CV Score (MSE): {:.2f}".format(mse.mean()))
+print("Average CV Score (MAE): {:.2f}".format(mae.mean()))
